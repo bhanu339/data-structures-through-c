ed1139cdf784331565cc2d30bc769fa86857f455
MANNEPALLI VEERA BHAVANI <bhanumannepalli7@gmail.com>
2026-04-26 20:28:27 +0530

    Add files via upload

---
+++ Reverse a SLL.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert a node at end
+void insert(struct Node** head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to reverse the linked list
+void reverse(struct Node** head) {
+    struct Node* prev = NULL;
+    struct Node* current = *head;
+    struct Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;   // store next node
+        current->next = prev;   // reverse link
+        prev = current;         // move prev forward
+        current = next;         // move current forward
+    }
+
+    *head = prev; // new head
+}
+
+// Function to display list
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int n, value;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        insert(&head, value);
+    }
+
+    printf("\nOriginal Linked List:\n");
+    display(head);
+
+    reverse(&head);
+
+    printf("\nReversed Linked List:\n");
+    display(head);
+
+    return 0;
+}
